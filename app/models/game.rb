aac4443e351b173b249774aab3b23e74a85587e0
OnyJ <julien.josselin75@hotmail.fr>
2020-06-16 15:06:51 +0200

    clean Game model file

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,8 +1,5 @@
 class Game < ApplicationRecord
-  # belongs_to :user, as :creator
-  # or
   belongs_to :user  # which is the creator
-
   has_many :histories
   has_many :users, through: :histories
   has_many :join_category_game
