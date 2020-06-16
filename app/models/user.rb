562ee64e26c87ddfe984a481ed7f9398c12d7755
OnyJ <julien.josselin75@hotmail.fr>
2020-06-16 12:08:48 +0200

    User model : validate username presence and uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable, 
          :jwt_authenticatable,
          jwt_revocation_strategy: JwtBlacklist
+
 end
