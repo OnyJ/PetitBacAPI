baab4e3aa399d142a101d4cdba9aa414374f17a3
OnyJ <julien.josselin75@hotmail.fr>
2020-06-16 13:38:49 +0200

    game needs a creator, can access categories - checked

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,10 +1,12 @@
 class Game < ApplicationRecord
+  # belongs_to :user, as :creator
+  # or
+  belongs_to :creator, class_name: "User"
 
-    # has_many :histories
-    # has_many :users, through: :histories
-    # has_many :join_category_game
-    # has_many :categories, through: :join_category_game
-    # has_many :invites
-    # has_many :invited_users, through: :invites, source: :users
-
+  # has_many :histories
+  # has_many :users, through: :histories
+  has_many :join_category_game
+  has_many :categories, through: :join_category_game
+  has_many :invites
+  has_many :invited_users, through: :invites, source: :users
 end
