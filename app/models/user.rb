683c13fb7891bc8a23d1486952da7f33cb0d96df
OnyJ <julien.josselin75@hotmail.fr>
2020-06-16 12:17:07 +0200

    set minimum username length to 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 3 }
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
