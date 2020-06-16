ef7b4b84b74d72bd0f3fcfa4d8919a656079ea9e
Robin Dercole <robin.dercole@gmail.com>
2020-06-16 12:28:11 +0200

    added join table category_game

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :join_category_games
   resources :games
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
