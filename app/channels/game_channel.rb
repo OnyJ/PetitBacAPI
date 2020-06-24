6f76b71e9ee4a6150307ef27ff907f8614e1fd9a
Robin Dercole <robin.dercole@gmail.com>
2020-06-24 21:36:39 +0200

    action cable wip

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,9 +1,22 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
+
+    puts '#############################################'
+    puts params
     stream_from "game_channel_#{params[:game_id]}"
+    puts "#############################################"
   end
   
   def received(data)
+    puts '*******************************************************'
+    puts data
+    puts params
+    puts '*******************************************************'
+    ActionCable.server.broadcast("game_channel_#{params[:game_id]}", data)
+  end
+
+  def stopping(data)
+    puts '*******************************************************'
     ActionCable.server.broadcast("game_channel_#{params[:game_id]}", data)
   end
 
